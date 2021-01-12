c1b4e7312a2a4e577c47d1506cdd9dc88b6ef95f
Dirkjan Ochtman <dirkjan@ochtman.nl>
2021-01-12 14:01:43 +0100

    Fix warnings in benchmarks

--- benches/build.rs
+++ benches/build.rs
@@ -11,14 +11,14 @@ use boomphf::*;
 fn build1_ser(bench: &mut Bencher) {
     bench.iter(|| {
         let items: Vec<u64> = (0..1000000u64).map(|x| x * 2).collect();
-        let phf = Mphf::new(2.0, &items);
+        let _ = Mphf::new(2.0, &items);
     });
 }
 
 fn build1_par(bench: &mut Bencher) {
     bench.iter(|| {
         let items: Vec<u64> = (0..1000000u64).map(|x| x * 2).collect();
-        let phf = Mphf::new_parallel(2.0, &items, None);
+        let _ = Mphf::new_parallel(2.0, &items, None);
     });
 }
 
